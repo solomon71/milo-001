bab8c4f35ab208a96616bcdbbdef307025fb806f
Paul Solomon <paul@thinkingbox.com>
2022-09-12 13:31:01 -0600

    testing paths

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,8 +16,7 @@ import { setLibs } from './utils.js';
 const STYLES = '/styles/styles.css';
 
 // Use '/libs' if your live site maps '/libs' to milo's origin.
-// const LIBS = 'https://milo.adobe.com/libs';
-const LIBS = '/libs';
+const LIBS = 'https://milo.adobe.com/libs';
 
 // Add any config options.
 const CONFIG = {
